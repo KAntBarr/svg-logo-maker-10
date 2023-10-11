981770f3b90c19cfdcdd036c39d91e75c3d059f5
Koby <kobysac@gmail.com>
2023-10-10 23:24:11 -0700

    added dependencies inquirer@8.2.4, jest

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hello World')
+const arr = [1,2,3,4,5];
+const obj = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5
+}
+
+for (index in arr) {
+    console.log("IN-Arr", index);
+}
+
+for (index in obj) {
+    console.log("IN-Obj", index);
+}
+
+for (index of arr) {
+    console.log("OF-Arr", index);
+}
