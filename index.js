e5e58ff167e0e53763192eca1a147c2ef4e1f134
Koby <kobysac@gmail.com>
2023-10-11 23:07:16 -0700

    altered file structure, added new class and test files

--- index.js
+++ index.js
@@ -1,20 +1,2 @@
-const arr = [1,2,3,4,5];
-const obj = {
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5
-}
-
-for (index in arr) {
-    console.log("IN-Arr", index);
-}
-
-for (index in obj) {
-    console.log("IN-Obj", index);
-}
-
-for (index of arr) {
-    console.log("OF-Arr", index);
-}
+const inquirer = require('inquirer');
+const { readFile, writeFile } = require('fs/promises');
