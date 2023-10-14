f4223670b8f03eb7800ca239da5ce24f3c337599
Koby <kobysac@gmail.com>
2023-10-14 02:06:19 -0700

    got Output to create an svg file, finish strong

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
-const input = require('./lib/Input.js');
-const output = require('./lib/Output.js');
+const Input = require('./lib/Input.js');
+const Creator = require('./lib/Creator.js')
+const Output = require('./lib/Output.js');
 
 function main() {
-    console.log(input);
-    input.begin()
+    // console.log(Input);
+    // console.log(new Input());
+    const promiseThenReturn = (new Input).begin()
         .then((data) => {
             // return output.write(data).then((result) => {
             //     console.log(result);
             // })
-            console.log(data);
+            // console.log(data);
+            // console.log(test);
+            // resolve("test");
+            // throw new Error("error...testing...error");
+            // return 'test';
+
+            const file = Creator.createSVG(data, );
+
+            Output.writeOut(file, '.svg', 'svg_file')
+                .then((result) => {
+                    console.log(result);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+
+        })
+        // .then((data) => {
+        //     console.log(data);
+        // })
+        .catch((err) => {
+            console.log(err);
         });
+    
+    // console.log("The return of a promise.then().catch():", promiseThenReturn);
 }
 
 main();
